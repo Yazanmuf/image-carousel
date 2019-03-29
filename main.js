53a715bb06cc7421a7c226e1a42517273b47e62d
Yazan Mufleh <ymufleh@gmail.com>
2019-03-29 12:15:24 -0400

    javascipt file, created the constants that will be used to track the current slide. created function that will move the current class name and append it either to next sibling element or previous sibling element

--- main.js
+++ main.js
@@ -5,3 +5,36 @@ const auto = false;
 const intervalTime = 5000;
 let slideInterval;
 
+const nextSlide = () => {
+
+    //Get current class
+    const current = document.querySelector('.current');
+    //Remove current class
+    current.classList.remove('current');
+    //check for next slide
+    if (current.nextElementSibling) {
+        //Add current to next sibling
+        current.nextElementSibling.classList.add('current');
+    } else {
+        //Add current to start
+        slides[0].classList.add('current')
+    }
+    setTimeout(() => current.classList.remove('current'));
+};
+
+const prevSlide = () => {
+
+    //Get current class
+    const current = document.querySelector('.current');
+    //Remove current class
+    current.classList.remove('current');
+    //check for previous slide
+    if (current.previousElementSibling) {
+        //Add current to previous sibling
+        current.previousElementSibling.classList.add('current');
+    } else {
+        //Add current to last
+        slides[slides.length - 1].classList.add('current')
+    }
+    setTimeout(() => current.classList.remove('current'));
+}
